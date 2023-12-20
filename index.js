69df7092b44d1f2619b8b741adddeb9ec843908c
Quyet_Thanh <115696569+thanhquyet24ptit@users.noreply.github.com>
2023-12-20 18:16:57 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 var map;
 var marker;
+var mainData;
+var nameToShow = null;
 
 const URLs = 'https://khkt-lc.onrender.com/home';
 
-/*------------------Load bản đồ và giao diện---------------------*/
-//Khởi tạo WEB
+function get_data() {
+    fetch(URLs)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then(data => {
+            mainData = data;
+        })
+        .catch(error => {
+            console.log("Lỗi truy vấn!", error);
+        });
+    // // console.log(name);
+    if (nameToShow != null) {
+        showInfor(nameToShow);
+    }
+}
+
 function init() {
+
     var options = {
         zoom: 13,
         center: { lat: 22.4809431, lng: 103.9754959 }
@@ -13,7 +34,6 @@ function init() {
 
     map = new google.maps.Map(document.getElementById('map'), options);
 
-    // console.log(name);
     fetch(URLs)
         .then(response => {
             if (!response.ok) {
@@ -39,11 +59,10 @@ function init() {
         })
         .catch(error => {
             console.log("Lỗi truy vấn!", error);
-        }); 
-}
+        });
 
-/*------------------Công cụ---------------------*/
-// Ẩn thanh thông tin
+    setInterval(get_data, 5000);
+}
 function hideToolsBar() {
     const toolsBar = document.getElementById('toolsBar');
     const image = document.getElementById('hideButtonIcon');
@@ -58,8 +77,8 @@ function hideToolsBar() {
         image.classList.add('flip-button-icon');
     }
 }
-// Hiển thị điểm vừa thêm lên UI
 function showInList(name, updatedAt) {
+    var container = document.querySelector('.container');
     let div = document.createElement('button');
 
     div.classList.add('new-point');
@@ -73,75 +92,58 @@ function showInList(name, updatedAt) {
         //onclick="deletePoint(this.parentNode,\'' + name + '\')
         html += '<button id="cancel-button-child-online"></button>';
         div.setAttribute('onclick', 'showInfor(\'' + name + '\')');
+        nameToShow = name;
     } else {
         html += '<button id="cancel-button-child-offline"></button>';
     }
     console.log(check(updatedAt));
     div.innerHTML = html;
-
-    var container = document.querySelector('.container');
     container.insertAdjacentElement('beforebegin', div);
 }
-// Lấy thông tin từ database
 function showInfor(name) {
     // console.log(name);
-    fetch(URLs)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(`HTTP error! Status: ${response.status}`);
-            }
-            return response.json();
-        })
-        .then(data => {
-            const targetData = data.find(item => item.name === name);
+    const targetData = mainData.find(item => item.name === name);
 
-            if (targetData) {
-                let number = parseFloat(targetData.thoiGianMua);
-                let numbertosend = number.toFixed(2);
-                
-                displayInfor(targetData.name,
-                    targetData.luongMua,
-                    targetData.doAm,
-                    targetData.doNghieng,
-                    numbertosend,
-                    targetData.satLo
-                )
-                
-                var lat = parseFloat(targetData.kinh);
-                var lng = parseFloat(targetData.viDo);
+    if (targetData) {
+        displayInfor(
+            targetData.name,
+            targetData.luongMua,
+            targetData.doAm,
+            targetData.doNghieng,
+            targetData.thoiGianMua,
+            targetData.satLo
+        );
 
-                var position = { lat, lng };
+        var lat = parseFloat(targetData.kinh);
+        var lng = parseFloat(targetData.viDo);
 
-                if (parseInt(targetData.satLo) == 4) {
-                    marker = new google.maps.Marker({
-                        position: position,
-                        map: map,
-                        icon: "./1.ico",
-                        title: targetData.name
-                    });    
-                } else {
-                    marker = new google.maps.Marker({
-                        position: position,
-                        map: map,
-                        icon: "./2.ico",
-                        title: targetData.name
-                    });    
-                }
+        var position = { lat, lng };
 
-                var latLng = new google.maps.LatLng(targetData.kinh, targetData.viDo);
-                map.panTo(latLng);
-            } else {
-                if (document.querySelector('table')) {
-                    removeTable();
-                }
-                console.log("Không tìm thấy điểm nào có tên như vậy!");
-            }
-        })
-        .catch(error => {
-            console.log("Lỗi truy vấn!", error);
-        });
+        if (parseInt(targetData.satLo) == 4) {
+            marker = new google.maps.Marker({
+                position: position,
+                map: map,
+                icon: "media\\1.ico",
+                title: targetData.name
+            });
+        } else {
+            marker = new google.maps.Marker({
+                position: position,
+                map: map,
+                icon: "media\\2.ico",
+                title: targetData.name
+            });
+        }
+
+        var latLng = new google.maps.LatLng(targetData.kinh, targetData.viDo);
+        map.panTo(latLng);
+    } else {
+        if (document.querySelector('table')) {
+            removeTable();
+        }
+        console.log("Không tìm thấy điểm nào có tên như vậy!");
+    }
 }
-// Hiển thị thông tin điểm
 function displayInfor(name, luongmua, doam, donghieng, rain_time, satLo) {
     if (document.querySelector('table')) {
         removeTable();
@@ -182,22 +184,93 @@ function displayInfor(name, luongmua, doam, donghieng, rain_time, satLo) {
     var container = document.querySelector('.show-point-infor');
     container.insertAdjacentElement('beforebegin', table);
 }
+function removeTable() {
+    var remove = document.querySelector('table');
+
+    if (remove && remove.parentNode) {
+        remove.parentNode.removeChild(remove);
+    } else {
+        console.error('Element not found or has no parent node');
+    }
+}
+function check(timeUpdate) {
+    var dateObject = new Date(timeUpdate);
+    var currentTime = new Date();
+
+    let time_in_second = currentTime.getSeconds() - dateObject.getSeconds();
+
+    if (time_in_second < 0 || time_in_second > 40) return false;
+    else return true;
+}
 
 
-/*------------------Popup thêm điểm---------------------*/
-// Hiển thị Popup
+
+function pushPointToDatabase(ten, kinhdo, vido) {
+    // const data = {
+    //     name: ten,
+    //     kinhDo: kinhdo,
+    //     viDo: vido,
+    //     doAm: '0',
+    //     luongMua: '0',
+    //     satLo: '0',
+    //     status: '1',
+    // };
+
+    // fetch(URLs, {
+    //     method: 'POST',
+    //     headers: { 'Content-Type': 'application/json' },
+    //     body: JSON.stringify(data)
+    // }).then(response => {
+    //     if (!response.ok) {
+    //         console.log(`HTTP error! Status: ${response.status}`);
+    //     } else {
+    //         console.log("Tải dữ liệu lên DataBase thành công!");
+    //     }
+    // })
+    // .catch(error => {
+    //     console.error('Error:', error);
+    // });
+}
+function deletePoint(elementToRemove, name) {
+    // fetch(URLs)
+    //     .then(response => {
+    //         if (!response.ok) {
+    //             console.log(`HTTP error! Status: ${response.status}`);
+    //         }
+    //         return response.json();
+    //     })
+    //     .then(data => {
+    //         const targetData = data.find(item => item.name === name);
+    //         console.log(targetData._id);
+
+    //         if (targetData) {
+    //             fetch(URLs + '/' + targetData._id, { method: 'DELETE' })
+    //                 .then(() => {
+    //                     displayInfor("", "", "", "", "");
+    //                 })
+    //         } else {
+    //             console.log("Không tìm thấy điểm nào có tên như vậy!");
+    //         }
+    //     })
+    //     .catch(error => {
+    //         console.log("Lỗi truy vấn!", error);
+    //     });
+    // if (elementToRemove && elementToRemove.parentNode) {
+    //     elementToRemove.parentNode.removeChild(elementToRemove);
+    // } else {
+    //     console.error('Element not found or has no parent node');
+    // }
+}
 function addPointPopup() {
     const popupToHide = document.getElementById('popup');
     if (popupToHide.classList.contains('hide-popup')) {
         popupToHide.classList.replace('hide-popup', 'add-point-popup');
     }
 }
-//Ẩn popup
 function hiddenPopup() {
     const popupToHide = document.getElementById('popup');
     popupToHide.classList.replace('add-point-popup', 'hide-popup');
 }
-//Thêm một điểm mới
 function addNewPoint() {
     var title = document.getElementById('titleOfPoint').value;
     var lat = parseFloat(document.getElementById('lat').value);
@@ -216,7 +289,7 @@ function addNewPoint() {
             document.getElementById('lat').value,
             document.getElementById('lng').value
         )
-        
+
         map.panTo(marker.getPosition());
         hiddenPopup();
         showInList(title);
@@ -226,86 +299,4 @@ function addNewPoint() {
     document.getElementById('titleOfPoint').value = ''
     document.getElementById('lat').value = ''
     document.getElementById('lng').value = ''
-}
-// Thêm điểm vào database
-function pushPointToDatabase(ten, kinh, vido) {
-    const data = {
-        name: ten,
-        kinh: kinh,
-        viDo: vido,
-        doAm: '0',
-        luongMua: '0',
-        satLo: '0',
-        status: '1',
-    };
-      
-    fetch(URLs, {
-        method: 'POST',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify(data)
-    }).then(response => {
-        if (!response.ok) {
-            console.log(`HTTP error! Status: ${response.status}`);
-        } else {
-            console.log("Tải dữ liệu lên DataBase thành công!");
-        }
-    })
-    .catch(error => {
-        console.error('Error:', error);
-    });
-}
-
-
-/*------------------Popup xóa điểm---------------------*/
-//Xóa một điểm
-function deletePoint(elementToRemove, name) {
-    fetch(URLs)
-        .then(response => {
-            if (!response.ok) {
-                console.log(`HTTP error! Status: ${response.status}`);
-            }
-            return response.json();
-        })
-        .then(data => {
-            const targetData = data.find(item => item.name === name);
-            console.log(targetData._id);
-
-            if (targetData) {
-                fetch (URLs + '/' + targetData._id, {method: 'DELETE'})
-                    .then (()=>{
-                        displayInfor("", "", "", "", "");
-                    })
-            } else {
-                console.log("Không tìm thấy điểm nào có tên như vậy!");
-            }
-        })
-        .catch(error => {
-            console.log("Lỗi truy vấn!", error);
-        });
-    if (elementToRemove && elementToRemove.parentNode) {
-        elementToRemove.parentNode.removeChild(elementToRemove);
-    } else {
-        console.error('Element not found or has no parent node');
-    }
-}
-// Xóa thông tin hiển thị
-function removeTable() {
-    var remove = document.querySelector('table');
-
-    if (remove && remove.parentNode) {
-        remove.parentNode.removeChild(remove);
-    } else {
-        console.error('Element not found or has no parent node');
-    }
-}
-
-// Check xem data có được cập nhật liên tục hay không
-function check(timeUpdate) {
-    var dateObject = new Date(timeUpdate);
-    var currentTime = new Date();
-
-    let time_in_second = currentTime.getSeconds() - dateObject.getSeconds();
-
-    if (time_in_second < 0 || time_in_second > 40 ) return false;
-    else return true;
 }
